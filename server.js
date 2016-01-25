5eac0308034e707ed7569021771875ad9dff0ad9
Francis Moisan <frankmoisan@hotmail.com>
2016-01-25 13:44:15 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var response = {unix: null, natural: null};
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.get('/:query', function(req, res) {
+	var query = new Date(req.params.query);
+	
+	if (Object.prototype.toString.call(query) === '[object Date]') {
+		if (!isNaN(query.getTime())) {
+			//var year = query.getFullYear();
+			//var month = query.getMonth();
+			//var day = query.getDate();
+			var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			//var naturalDate = months[month] + ' ' + day + ', ' + year;
+			
+			response.unix = query.getTime();
+			response.natural = months[query.getMonth()] + ' ' + query.getDate() + ', ' + query.getFullYear();
+		}
+	}
+	
+	res.writeHead(200, { 'Content-Type': 'application/json'});
+	res.end(JSON.stringify(response));
 });
+
+app.get('/', function(req, res) {
+	var index = 'Hello world!';
+	res.writeHead(200, { 'Content-Type': 'application/json'});
+	res.end(index);
+});
+
+app.listen(8080);
